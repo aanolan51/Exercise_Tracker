a4e60532bd129e1de8a568c6471b694aae1938e4
aanolan51 <a.a.nolan@hotmail.com>
2021-07-29 13:33:36 -0700

    Started api Routes file.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,21 +5,34 @@ const Exercise = require("../models/Exercise");
 
 
 //Add a new exercise to a new workout plan:
+//path from the api.js file in the public folder:
+router.post("/api/workouts", ({ body }, res) => {
+    // console.log(body);
+
+    Exercise.create(body)
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
 
 
 //View combined weight of multiple exercises from past 7 workouts on stats page:
+router.get("/api/workouts", (req, res) => {
+    Exercise.find({})
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
 
 //View total duration of of each workout from the past 7 workouts on the stats page:
 
-// router.post("/api/transaction", ({ body }, res) => {
-//   Exercise.create(body)
-//     .then(dbExercise => {
-//       res.json(dbExercise);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes that will be used:
-// app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
